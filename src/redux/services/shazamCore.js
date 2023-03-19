ffc77cf303b40e65f599bd17fb8b82fcd61513e9
utkarsh1212-i <utkarshui1212@gmail.com>
2023-03-19 17:11:51 +0530

    Top charts top artist and Search Bar added

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -2,6 +2,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const url = "https://shazam-core.p.rapidapi.com/v1/charts/world";
 
+// export const shazam2Api = createApi({
+//   reducerPath: "shazam2Api",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: "https://shazam8.p.rapidapi.com",
+//     prepareHeaders: (headers) => {
+//       headers.set(
+//         "X-RapidAPI-Key",
+//         "99241e245cmsh60843fca1f55fd1p1616c2jsn649f6ef7e19e"
+//       );
+//       return headers;
+//     },
+//   }),
+//   endpoints: (builder) => ({
+//     getSongsByCountry : builder.query({
+//       query: ({ songid }) => `/track/top/country?country_code=${country}&limit=10`,
+//     }),
+//   })
+// })
+
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
@@ -22,9 +41,19 @@ export const shazamCoreApi = createApi({
     getSongRelated: builder.query({
       query: ({ songid }) => `/songs/list-recommendations?key=${songid}`,
     }),
-    getArtistsDetails : builder.query({ query : (artistId) => `/artists/get-top-songs?id=${artistId}`})
+    getArtistsDetails: builder.query({
+      query: (artistId) => `/artists/get-top-songs?id=${artistId}`,
+    }),
+    getSongsByCountry: builder.query({
+      query: (countryCode) => `/charts/country/country_code=${countryCode}`,
+    }),
   }),
-  // getSongsByCountry : 
 });
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistsDetailsQuery } = shazamCoreApi;
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistsDetailsQuery,
+  useGetSongsByCountry
+} = shazamCoreApi;
